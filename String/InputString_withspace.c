76921d930b480452064e1df56eb8f469020019e5
toqitahmid <rubaiathali@gmail.com>
2025-10-01 21:24:54 +0600

    Added input output file

--- String/InputString_withspace.c
+++ String/InputString_withspace.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 int main(){
 
+     #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
     char name[100];
 
-    printf("Enter your name : ");
     fgets(name,sizeof(name),stdin);
     printf("%s", name);
 }
