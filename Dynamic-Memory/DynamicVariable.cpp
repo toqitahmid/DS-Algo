212b799dc7ba5fd63945b8460449b833ac2e2022
toqitahmid <rubaiathali@gmail.com>
2025-10-02 12:47:54 +0600

    Creat dynamic variable

---
+++ Dynamic-Memory/DynamicVariable.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int* value = new int;
+    *value = 10;
+    cout << *value;
+    return 0;
+}
